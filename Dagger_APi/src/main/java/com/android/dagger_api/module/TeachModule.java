40d25d7409a8d906fcefe5a1d6742ffbfc6783dd
zenglanjing <lanjing.zeng@shopee.com>
2021-04-13 10:09:14 +0800

    Dagger_APi demo完善

--- Dagger_APi/src/main/java/com/android/dagger_api/module/TeachModule.java
+++ Dagger_APi/src/main/java/com/android/dagger_api/module/TeachModule.java
@@ -13,8 +13,21 @@ public abstract class TeachModule {
     abstract ITeach getTeach(MultiMediaTeach multiMediaTeach);
 
     @Provides
-    static MultiMediaTeach getMultiMediaTeacher(){
+    static MultiMediaTeach getMultiMediaTeacher() {
         return new MultiMediaTeach();
     }
 
 }
+
+//import com.android.dagger_api.teach.ITeach;
+//import com.android.dagger_api.teach.MultiMediaTeach;
+//
+//import dagger.Binds;
+//import dagger.Module;
+//
+//@Module(includes = {MultiMediaTeachModel.class})
+//public abstract class TeachModule {
+//    @Binds
+//    abstract ITeach getTeach(MultiMediaTeach multiMediaTeach);
+//}
+
